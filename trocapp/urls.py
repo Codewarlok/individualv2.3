9caffa20a4c2549ea644b11414721e9b7512d63f
Codewarlok <negradiuca@gmail.com>
2022-07-02 08:20:39 -0400

    carga2

--- trocapp/urls.py
+++ trocapp/urls.py
@@ -1,7 +1,7 @@
-"""trocapp URL Configuration
+"""webplayground URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from pages.urls import pages_patterns
+from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
+from django.conf import settings
 
 urlpatterns = [
+    path('', include('core.urls')),
+    path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    # Paths de Auth
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
+    # Paths de profiles
+    path('profiles/', include(profiles_patterns)),
+    # Paths de Messenger
+    path('messenger/', include(messenger_patterns)),
+
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
